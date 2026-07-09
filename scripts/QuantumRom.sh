c3b81fe26cf8b1d528fe3ccf5acfaba81e2e3890
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2026-07-09 21:19:47 +0600

    Update QuantumRom.sh

--- scripts/QuantumRom.sh
+++ scripts/QuantumRom.sh
@@ -766,7 +766,7 @@ PATCH_FLAG_SECURE() {
     REPLACE_SMALI_METHOD "$FILE_1" "$METHOD_NAME_1" "$REPLACE_BODY_1"
 
 	local FILE_2="${1}/smali_classes2/com/android/server/wm/WindowManagerService.smali"
-    local METHOD_NAME_2=".method public final notifyScreenshotListeners(I)Ljava/util/List;"
+    local METHOD_NAME_2=".method public notifyScreenshotListeners(I)Ljava/util/List;"
     local REPLACE_BODY_2='
     .locals 3
     .annotation system Ldalvik/annotation/Signature;
@@ -805,7 +805,7 @@ PATCH_FLAG_SECURE() {
 
     throw p0
     '
-    # REPLACE_SMALI_METHOD "$FILE_2" "$METHOD_NAME_2" "$REPLACE_BODY_2"
+    REPLACE_SMALI_METHOD "$FILE_2" "$METHOD_NAME_2" "$REPLACE_BODY_2"
 }
 
 
